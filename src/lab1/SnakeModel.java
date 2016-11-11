c7578c65fde2dffafcdfa5c1650caec36e15b0f3
agerhall <sebbe.agerhall@hotmail.com>
2016-11-11 10:42:37 +0100

    laddade upp skiten

---
+++ src/lab1/SnakeModel.java
@@ -0,0 +1,15 @@
+package lab1;
+
+public class SnakeModel extends GameModel{
+	
+	public enum Directions {
+		NORTH(0,-1),
+		SOUTH(0,1),
+		EAST(1,0),
+		WEST(-1,0),
+		NONE(0,0);
+	}
+	
+	public SnakeModel
+
+}
